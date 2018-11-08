18e61d0ca76185220145d87831252ff0935b6c08
QEver <QEver.cn@gmail.com>
2018-11-08 22:03:39 +0800

    add version

--- opengrok.py
+++ opengrok.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import os
+import re
 import sys
-import shutil
 import platform
 import zipfile
 import subprocess
@@ -20,7 +20,7 @@ elif platform.system() == 'Linux':
     cmd = ["whereis", "ctags"]
     CTAGS_PATH = run_cmd(cmd)[0].split(":")[1].strip().split(" ")[0]
     TOMCAT_DIR = '/var/lib/tomcat8'
-    OPENGROK_DIR = os.path.expanduser('~/tools/opengrok')
+    OPENGROK_DIR = os.path.expanduser('~/tools/opengrok-1.1-rc58')
 elif platform.system() == 'Darwin':
     cmd = ['brew', '--prefix', 'tomcat@8.0']
     TOMCAT_DIR = run_cmd(cmd)[0].strip()
@@ -31,6 +31,7 @@ elif platform.system() == 'Darwin':
 else:
     print "Unsupport Platform : ", platform.system()
     exit()
+
 
 OPENGROK_OPTIONS = '-r on -a on -S -P -C'
 JAVA_OPTIONS = '-Xmx4096m'
@@ -72,13 +73,42 @@ print 'WebApps Dir : %s' % WEBAPPS_DIR
 print 
 print 'Ctags Path : %s' % CTAGS_PATH
 
+
+def get_opengrok_version():
+    cmd = 'java ' + JAVA_OPTIONS + ' -jar ' + OPENGROK_JAR + ' -V'
+    vers = os.popen(cmd).read()
+    regexp = 'OpenGrok (.+) rev'
+    g = re.search(regexp, vers).groups()
+    if len(g) != 1:
+        raise Exception('Can not get version of opengrok\n, version data is : %s' % vers)
+    return g[0]
+
+
+def get_opengrok_indexer():
+    return os.path.join(OPENGROK_DIR, 'bin/indexer.py')
+
+def is_opengrok_has_indexer():
+    path = get_opengrok_indexer()
+    if os.path.exists(path):
+        return True
+    return False
+
+
 def run_opengrok(path, name):
     cmd = ''
-    cmd += 'java ' + JAVA_OPTIONS + ' '
-    cmd += '-jar ' + OPENGROK_JAR + ' '
-    cmd += OPENGROK_OPTIONS + ' '
-    cmd += '-c ' + CTAGS_PATH + ' '
-    cmd += '-w ' + name +' '
+    version = get_opengrok_version()
+    if version > 'v1.1-rc11' and is_opengrok_has_indexer():
+        indexer = get_opengrok_indexer()
+        #./indexer.py -a ../lib/opengrok.jar -- -s /home/qever/source/v8/ -d ../data -W ../data/v8.xml -U http://localhost:8080
+        cmd += indexer + ' '
+        cmd += '-a ' + OPENGROK_JAR + ' -- '
+    else:
+        cmd += 'java ' + JAVA_OPTIONS + ' '
+        cmd += '-jar ' + OPENGROK_JAR + ' '
+        cmd += OPENGROK_OPTIONS + ' '
+        cmd += '-c ' + CTAGS_PATH + ' '
+        cmd += '-w ' + name +' '
+    
     cmd += '-W ' + os.path.join(OPENGROK_DATA, name + '.xml') + ' '
     cmd += '-d ' + os.path.join(OPENGROK_DATA, name) + ' '
     cmd += '-s ' + path
@@ -87,10 +117,31 @@ def run_opengrok(path, name):
     os.system(cmd)
 
 
+def new_context_param(name, value):
+    context = ET.Element('ns0:context-param')
+
+    param_name = ET.Element('ns0:param-name')
+    param_name.text = name
+    param_value = ET.Element('ns0:param-value')
+    param_value.text = value
+
+    context.append(param_name)
+    context.append(param_value)
+    return context
+
+
 def addto_webxml(xml, config, src_root, data_root):
     ET.register_namespace('', 'http://java.sun.com/xml/ns/javaee')
     ET.register_namespace('xsi', 'http://www.w3.org/2001/XMLSchema-instance')
-    tree = ET.fromstring(xml)
+    try:
+        tree = ET.fromstring(xml)
+    except ImportError,e:
+        print e
+        from xml.etree import ElementTree
+        from elementtree import SimpleXMLTreeBuilder
+        ElementTree.XMLTreeBuilder = SimpleXMLTreeBuilder.TreeBuilder
+        tree = ET.fromstring(xml)
+
 
     for child in tree:
         if 'context-param' in child.tag:
@@ -100,26 +151,27 @@ def addto_webxml(xml, config, src_root, data_root):
                         for j in child:
                             if 'param-value' in j.tag:
                                 j.text = config
+    
+    src_root_elem = new_context_param('SRC_ROOT', src_root)
+#    data = '''
+#  <context-param>
+#   <param-name>SRC_ROOT</param-name>
+#   <param-value>%s</param-value>
+#  </context-param>
+#  ''' % src_root
+#
+#    e = ET.fromstring(data)
+    data_root_elem = new_context_param('DATA_ROOT', data_root)
+#    data = '''
+#  <context-param>
+#   <param-name>DATA_ROOT</param-name>
+#   <param-value>%s</param-value>
+#  </context-param>''' % data_root
+#
+#    t = ET.fromstring(data)
 
-    data = '''
-  <context-param>
-   <param-name>SRC_ROOT</param-name>
-   <param-value>%s</param-value>
-  </context-param>
-  ''' % src_root
-
-    e = ET.fromstring(data)
-
-    data = '''
-  <context-param>
-   <param-name>DATA_ROOT</param-name>
-   <param-value>%s</param-value>
-  </context-param>''' % data_root
-
-    t = ET.fromstring(data)
-
-    tree.insert(3, e)
-    tree.insert(3, t)
+    tree.insert(3, src_root_elem)
+    tree.insert(3, data_root_elem)
 
     return ET.tostring(tree, method='xml')
 
@@ -170,7 +222,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         path = sys.argv[1]
     else:
-	path = os.path.abspath(".")
+        path = os.path.abspath(".")
     name = os.path.basename(path)
 
     run_tomcat(name, path)
